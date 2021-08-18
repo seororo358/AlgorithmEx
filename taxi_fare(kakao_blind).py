b13aa090e04476427d2c0308a6c4b78b386a3145
seororo358 <ssr3588@nate.com>
2021-08-18 09:03:48 +0900

    taxi_fare_correct

--- taxi_fare(kakao_blind).py
+++ taxi_fare(kakao_blind).py
@@ -1,29 +1,31 @@
+import math
+## 플로이드 와샬
+import math
+
+
 def solution(n, s, a, b, fares):
+    INF = math.inf
+    mapping = [[INF] * n for _ in range(n)]
 
-    INF = 1e7
-
-    mapping = [[INF] * (n + 1) for _ in range(n + 1)]
-    for i in range(0, n + 1):
+    for i in range(0, n):
         mapping[i][i] = 0
-    for i in fares:
-        mapping[i[0]][i[1]], mapping[i[1]][i[0]] = i[2], i[2]
 
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if mapping[i][j] == INF:
-                continue
-            for k in range(1, n + 1):
-                if mapping[i][j] + mapping[j][k] < mapping[i][k]:
-                    mapping[i][k] = mapping[i][j] + mapping[j][k]
-                    mapping[k][i] = mapping[i][k]
+    for i in fares:
+        mapping[i[0] - 1][i[1] - 1], mapping[i[1] - 1][i[0] - 1] = i[2], i[2]
+    for k in range(0, n):
+        for i in range(0, n):
+            for j in range(0, n):
+                if mapping[i][j] > mapping[i][k] + mapping[k][j]:
+                    mapping[i][j] = mapping[i][k] + mapping[k][j]
+                    mapping[j][i] = mapping[i][j]
                 else:
                     pass
 
-    not_with = mapping[a][s] + mapping[b][s]
-    with_ma = min(mapping[a][i] + mapping[b][i] + mapping[s][i] for i in range(1,n+1))
-    if not_with < with_ma:
-        return not_with
-    else:
-        return with_ma
+    return min([mapping[a - 1][i] + mapping[b - 1][i] + mapping[s - 1][i] for i in range(0, n)])
+
+    ##if not_with < with_ma:
+    ##    return not_with
+    ##else:
+    ##    return with_ma###
 
 print(solution(6,4,6,2,[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
