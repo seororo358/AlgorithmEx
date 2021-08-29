fc75bf2eac0e63da1a932ea16d92e3fc4f0860ca
seororo358 <ssr3588@nate.com>
2021-08-29 13:21:05 +0900

    toothbrush_second

--- toothbrush.py
+++ toothbrush.py
@@ -11,3 +11,8 @@ def solution(enroll, referral, seller, amount):
 
     answer = list(money.values())
     return answer
+
+print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
+               ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],
+               ["young", "john", "tod", "emily", "mary"],
+               [12, 4, 2, 5, 10]))
