a65f0212b7cabf602af838ee3d2eb3ef42f016f9
seororo358 <ssr3588@nate.com>
2021-08-29 12:53:45 +0900

    toothbrush_second

--- toothbrush.py
+++ toothbrush.py
@@ -1,6 +1,13 @@
 def solution(enroll, referral, seller, amount):
-    answer = []
+
+    money = {}
+    for i in enroll:
+        money[i] = 0
+
     for i in range(0,len(amount)):
         amount[i] *= 100
-    
+
+
+
+    answer = list(money.values())
     return answer
