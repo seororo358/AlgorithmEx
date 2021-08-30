32de4950285a6302157a8bc8c647ab0e8b7209c4
seororo358 <ssr3588@nate.com>
2021-08-30 16:23:01 +0900

    toothbrush_last

--- toothbrush.py
+++ toothbrush.py
@@ -1,16 +1,32 @@
 def solution(enroll, referral, seller, amount):
-
     money = {}
+    en_refer = {}
+
+    def tracking(child, per):
+        if child == "-" or per == 0:
+            return
+        else:
+            money[child] += per - per//10
+            tracking(en_refer[child], per//10)
+
     for i in enroll:
         money[i] = 0
+
+    for i,j in zip(enroll,referral):
+        en_refer[i] = j
 
     for i in range(0,len(amount)):
         amount[i] *= 100
 
-
+    for i in range(len(seller)):
+        mon = amount[i]//10
+        money[seller[i]] += amount[i] - mon
+        tracking(en_refer[seller[i]], mon)
 
     answer = list(money.values())
+
     return answer
+
 
 print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
                ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],
