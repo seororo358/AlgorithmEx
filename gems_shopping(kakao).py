b0a4bb47992cc301cb86b818b97e065dc0a40a6a
seororo358 <ssr3588@nate.com>
2021-09-04 03:12:50 +0900

    gems_renew_last

--- gems_shopping(kakao).py
+++ gems_shopping(kakao).py
@@ -3,21 +3,37 @@ def solution(gems):
     leng_set = len(gemset)
     gem_dic = {}
     leng_dic = 0
-    last = 0
     start = 0
+    found_all = False
+    q = []
 
     for i in range(len(gems)):
         if gems[i] not in gem_dic.keys():
             gem_dic[gems[i]] = 1
-
-
+            leng_dic += 1
+            if leng_dic == leng_set:
+                found_all = True
+                last = i+1
+                for lo in range(start, last):
+                    gem_dic[gems[lo]] -= 1
+                    if gem_dic[gems[lo]] == 0:
+                        start = lo + 1
+                        q.append((abs(last - start), start, last))
+                        break
         else:
             gem_dic[gems[i]] += 1
+            if gem_dic[gems[i]] == 1 and found_all:
+                last = i+1
+                for lo in range(start, last):
+                    gem_dic[gems[lo]] -= 1
+                    if gem_dic[gems[lo]] == 0:
+                        start = lo + 1
+                        q.append((abs(last - start), start, last))
+                        break
 
+    q.sort(key=lambda x: x[0])
 
-
-
-    answer = [start, last]
+    answer = [q[0][1], q[0][2]]
 
     return answer
 
