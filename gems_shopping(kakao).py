c81bef495793846e446a20bb7024eb46623a6793
seororo358 <ssr3588@nate.com>
2021-09-03 22:38:36 +0900

    gems_renew_1st

--- gems_shopping(kakao).py
+++ gems_shopping(kakao).py
@@ -1,29 +1,25 @@
 def solution(gems):
-    gemset = len(set(gems))
+    gemset = set(gems)
+    leng_set = len(gemset)
     gem_dic = {}
-    last = 1
-    start = 1
+    leng_dic = 0
+    last = 0
+    start = 0
 
-    for gem in gems:
-        if gem not in gem_dic.keys():
-            gem_dic[gem] = 1
-        else:
-            gem_dic[gem] += 1
-        if 0 not in gem_dic.values() and gemset == len(gem_dic.keys()):
-            break
-        else:
-            last += 1
+    for i in range(len(gems)):
+        if gems[i] not in gem_dic.keys():
+            gem_dic[gems[i]] = 1
 
-    for gem in gems:
-        gem_dic[gem] -= 1
-        if gem_dic[gem] == 0:
-            break
+
         else:
-            start += 1
+            gem_dic[gems[i]] += 1
+
+
+
 
     answer = [start, last]
 
     return answer
 
 
-print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA", "RUBY"]))
