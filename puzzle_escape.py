75c36a298020ac7613a5cb301fe9b2712100b85e
seororo358 <ssr3588@nate.com>
2021-09-12 13:06:04 +0900

    temp

--- puzzle_escape.py
+++ puzzle_escape.py
@@ -7,6 +7,7 @@ def solution(n, start, end, roads, traps):
     for i in range(1, n + 1):
         graph[i][i] = 0
 
+
     for edge in roads:
         if graph[edge[0]][edge[1]] > edge[2]:
             graph[edge[0]][edge[1]] = edge[2]
@@ -18,7 +19,9 @@ def solution(n, start, end, roads, traps):
 
 def trap_on(graph, trap_num):
     for i in range(len(graph)):
+        i += 1
 
+    return
 
 
 print(solution(3,1,3,[[1,2,2],[3,2,3]],[2]))
