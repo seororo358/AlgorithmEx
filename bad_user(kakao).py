9ed9a9d0eace18f63dd11e6bb82279e71f5f2a1b
seororo358 <ssr3588@nate.com>
2021-09-16 23:43:22 +0900

    bad_user(kakao)_last

--- bad_user(kakao).py
+++ bad_user(kakao).py
@@ -1,6 +1,9 @@
 from itertools import product as pr
+from itertools import combinations as comb
+
 def solution(user_id, banned_id):
     answer = 0
+    answer_set = set()
     ban = dict()
     for user in user_id:
         for banned in banned_id:
@@ -17,15 +20,28 @@ def solution(user_id, banned_id):
                     else:
                         pass
             if true_tb:
-                if banned not in ban.keys():
-                    ban[banned] = set()
-                    ban[banned].add(user)
+                if user not in ban.keys():
+                    ban[user] = set()
+                    ban[user].add(banned)
                 else:
-                    ban[banned].add(user)
-    st = list(ban.values())
+                    ban[user].add(banned)
+
+    for user in ban.keys():
+        ban[user] = list(ban[user])
+
+    for combi in comb(ban.keys(),len(banned_id)):
+        st = []
+        for temp in combi:
+            st.append(ban[temp])
+
+        for prod in list(pr(*st)):
+            if sorted(banned_id) == sorted(prod):
+                answer_set.add(tuple(sorted(combi)))
+
+    answer = len(answer_set)
+
+    return answer
 
 
-    return st
-
-
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["fr*d*", "*rodo", "******", "******"]))
+print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],
+                ["fr*d*", "abc1**"]))
