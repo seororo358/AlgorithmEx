ea56b1bf66969335e81e042b0654ac919b8f7d6a
seororo358 <ssr3588@nate.com>
2021-09-18 03:54:42 +0900

    rock_jump(kakao)_last

--- rock_jump(kakao).py
+++ rock_jump(kakao).py
@@ -2,11 +2,12 @@ def solution(stones, k):
     answer = 0
     max_c = 200000001
     min_c = 1
+    #rock 이분탐색 시작
     while max_c >= min_c:
         mid = (max_c + min_c) // 2
         count = 0
         is_true = True
-        for i in stones:
+        for i in stones:#이분탐색 시작시 통과사람수가 mid보다 많을경우
             if i - mid <= 0:
                 count += 1
                 if count == k:
@@ -14,6 +15,7 @@ def solution(stones, k):
                     break
             else:
                 count = 0
+        #이분탐색 결과 정리
         if is_true:
             min_c = mid + 1
             answer = min_c
