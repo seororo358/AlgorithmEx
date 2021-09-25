2a62d060603db14912e3f6b6b751a79cee2dffb1
seororo358 <ssr3588@nate.com>
2021-09-25 19:42:21 +0900

    tem

--- 2022kakao_2round.py
+++ 2022kakao_2round.py
@@ -1,135 +1,187 @@
-import json
 import requests
 import random
+import json
+
+scom = ['not', 'up', 'right', 'down', 'left', 'in', 'out']
+dr = [0, -1, 0, 1, 0, 0, 0]
+dc = [0, 0, 1, 0, -1, 0, 0]
+tcommand = {scom[i]: i for i in range(7)}
 
 
-def renew_grade(user, win, lose, time):
-    score = (40-time) * 99000 * 35 // 100000
-    user[win] += score
-    user[lose] = max(user[lose] - score, 0)
+class truck:
+    def __init__(self):
+        self.p = 0
+        self.load = 0
 
-    print("renew")
+    def __str__(self):
+        return str(self.p) + ' ' + str(self.load)
 
-def define_match(user, wait):
-    pair = []
-    while len(wait) > 1:
-        f = wait.pop()
-        t = wait.pop()
-        pair.append([f[0], t[0]])
-    print("define")
-
-    return pair
-
-def Gameresultapi(url,headers):
-    path = 'game_result'
-    res = []
-    req = requests.get(url+'/'+path, headers = headers)
-    j = req.json()
-    for w, l, time in [(i['win'], i['lose'], i['taken']) for i in j['game_result']]:
-        res.append((w, l, time))
-
-    return res
+    def __repr__(self):
+        return '(' + str(self.p) + ' ' + str(self.load) + ')'
 
 
-def Waitingapi(url, headers):
-    path = 'waiting_line'
+def getlocation(url, headers, bycycle):
+    path = 'locations'
+    req = requests.get('/'.join([url, path]), headers=headers)
 
-    arr = []
-    req = requests.get(url+'/'+path, headers=headers)
     j = req.json()
 
-    for id, fr in [(i['id'], i['from']) for i in j['waiting_line']]:
-        arr.append((id, fr))
+    for id, cnt in [(i['id'], i['located_bikes_count']) for i in j['locations']]:
+        bycycle[id] = cnt
 
-    return arr
+    return bycycle
 
 
-def Userinfoapi(url, headers, users):
-    path = 'user_info'
+def gettrucks(url, headers, trucks):
+    path = 'trucks'
+    req = requests.get('/'.join([url, path]), headers=headers)
 
-    req = requests.get(url+'/'+path, headers=headers)
     j = req.json()
-    for id, grade in [(i['id'], i['grade']) for i in j['user_info']]:
-        users[id] = grade
+    for id, p, cnt in [(i['id'], i['location_id'], i['loaded_bikes_count']) for i in j['trucks']]:
+        trucks[id].p = p
+        trucks[id].load = cnt
 
-    return users
-
-
-#data = {'pairs':[[],,,,,]}
-
-def Matchapi(url, headers, data):
-    path = 'match'
-
-    req = requests.put(url + '/' + path, headers=headers, data=data)
-    j = req.json()
-
-    return j
+    return trucks
 
 
-#data = [{'id': 1, 'grade': 1900},,,,,,]
+def simulate(url, header, data):
+    path = 'simulate'
+    req = requests.put('/'.join([url, path]), headers=header, data=data)
 
-def Changegradeapi(url, headers, data):
-    path = 'change_grade'
-
-    req = requests.put(url + '/' + path, headers=headers, data=data)
-    j = req.json()
-
-    return j
+    return req.json()
 
 
-def main(qid):
-    url = 'https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod'
-    token = '90c097a3819eef80cdfd5c594b3c0fc1'
+def getdist(loc, f, t):
+    dcom = [tcommand['up'] for i in range(abs(loc[f][0] - loc[t][0])) if loc[f][0] > loc[t][0]]
+    dcom.extend([tcommand['down'] for i in range(abs(loc[f][0] - loc[t][0])) if loc[f][0] < loc[t][0]])
+    dcom.extend([tcommand['right'] for i in range(abs(loc[f][1] - loc[t][1])) if loc[f][1] < loc[t][1]])
+    dcom.extend([tcommand['left'] for i in range(abs(loc[f][1] - loc[t][1])) if loc[f][1] > loc[t][1]])
+
+    return (abs(loc[f][0] - loc[t][0]) + abs(loc[f][1] - loc[t][1]), dcom)
+
+
+def truckmove(t, bnum, map, mmap, mean, comm, dest, all='not'):
+    x, y = map[t.p][0], map[t.p][1]
+    retcomm = []
+
+    for i in comm:
+        if len(retcomm) >= 10: break
+        id = mmap[x][y]
+
+        if id == dest and all == 'out':
+            need = t.load
+            retcomm.extend([tcommand['out'] for i in range(need) if need > 0])
+            bnum[id] += need
+        elif all == 'in' and id == dest:
+            need = min(bnum[id] - mean, 20 - t.load)
+            retcomm.extend([tcommand['in'] for i in range(need) if need > 0])
+            bnum[id] -= need
+        elif all == 'out':
+            need = min(bnum[id] - mean, 20 - t.load)
+            retcomm.extend([tcommand['in'] for i in range(need) if need > 0])
+            bnum[id] -= need
+        elif all == 'in':
+            need = min(mean - bnum[id], t.load)
+            retcomm.extend([tcommand['out'] for i in range(need) if need > 0])
+            bnum[id] += need
+
+        else:
+            if bnum[id] < mean:
+                need = min(mean - bnum[id], t.load)
+                retcomm.extend([tcommand['out'] for i in range(need) if need > 0])
+                bnum[id] += need
+            elif bnum[id] > mean:
+                need = min(bnum[id] - mean, 20 - t.load)
+                retcomm.extend([tcommand['in'] for i in range(need) if need > 0])
+                bnum[id] -= need
+
+        retcomm.append(i)
+        x += dr[i]
+        y += dc[i]
+
+    return retcomm
+
+
+def kakaotaxi(qid=1):
+    url = 'https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users'
     path = 'start'
-    headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+    token = '40183910509330daf1b31650b8aff7bb'
     param = {'problem': qid}
+    headers = {'X-Auth-Token': token, 'Content_Type': 'application/json'}
+
+    msize = 0
+    mean = 15
 
     if qid == 1:
-        count = 30
-        mean = 1
+        msize = 5
+        mean = 2
+        emer = 4
     else:
-        count = 900
-        mean = 45
+        msize = 60
+        mean = 3
+        emer = 6
 
-    user = [int(random.uniform(0, 9999)) for _ in range(count+1)]
-    #playing = [0 for _ in range(count+1)]
+    mymap = [[msize-i-1 + msize*j for j in range(msize)] for i in range(msize)]
+
+    pos = {mymap[i][j]: (i, j) for i in range(msize) for j in range(msize)}
+
+    bycycles = [0 for i in range(msize*msize)]
+    tnum = [0, 5, 10]
+    trucks = [truck() for i in range(tnum[qid])]
+    req = requests.post('/'.join([url, path]), headers=headers, data=json.dumps(param))
+    j = req.json()
+    auth_key = j['auth_key']
+    headers = {'Authorization': auth_key, 'Content-Type': 'applications/json'}
+
+    next_command = [[] for i in range(len(trucks))]
+    bnum = [0, 4, 3]
+    prev_bycycle = [bnum[qid] for i in range(msize*msize)]
+
+    for i in range(720):
+        trucksdes = [int(random.uniform(0, msize*msize)) for i in range(len(trucks))]
+        bycycles = getlocation(url, headers, bycycle=bycycles)
+        mmin = 100000000
+        mini = 0
+        mmax = -100000000
+        maxi = 0
+
+        for i, j in enumerate([prev_bycycle[i] - bycycles[i] for i in range(len(bycycles))]):
+            if mmin > j:
+                mmin, mini = j, i
+            if mmax < j:
+                mmax, maxi = j, i
+
+        trucks = gettrucks(url, headers, trucks)
+        emergen = [i[0] for i in enumerate(bycycles) if i[1] == 0 or i[1] > emer]
+
+        i = 0
+
+        while i < min(int(mmin/20), tnum[qid]):
+            trucksdes[i] = mini
+            i += 1
+        while i < min(int(mmax/20), tnum[qid]):
+            trucksdes[i] = maxi
+            i += 1
+
+        for i in range(tnum[qid]):
+            t = trucks[i]
+            if len(emergen) > 0:
+                trucksdes[i] = emergen.pop()
+            idx, next_command[i] = getdist(pos, t.p, trucksdes[i])
+
+        nextcommand = []
+
+        for i in range(tnum[qid]):
+            t = trucks[i]
+            ncom = truckmove(t, bycycles, pos, mymap, mean, next_command[i], -1, 'not')
+            nextcommand.append({'truck_id': i, 'command': ncom})
+
+        j = simulate(url, header=headers, data=json.dumps({'commands': nextcommand}))
+        print(j['time'], j['failed_requests_count'])
+        prev_bycycle = [i for i in bycycles]
+    req = requests.get(url + '/score', headers=headers)
+    print(str(req.json()['score']))
 
 
-    req = requests.post(url + '/' + path, headers=headers, data=json.dumps(param))
-    js = req.json()
-    auth_key = js['auth_key']
-    headers = {'Authorization': auth_key, 'Content-Type': 'application/json'}
-
-
-    for i in range(596):
-        user = Userinfoapi(url, headers=headers, users=user)
-        if i < 555:
-            wait_q = []
-            wait_q = Waitingapi(url, headers=headers)
-
-            pair = define_match(user, wait_q)
-            js = Matchapi(url, headers=headers, data=json.dumps({'pairs': pair}))
-            print(js['time'])
-        res = Gameresultapi(url, headers=headers)
-        commands = []
-        while res and len(commands) <= 10:
-            w, l, t = res.pop()
-            renew_grade(user, w, l, t)
-            commands.append({'id': w, 'grade': user[w]})
-            commands.append({'id': l, 'grade': user[l]})
-
-        js = Changegradeapi(url, headers=headers, data=json.dumps({'commands': commands}))
-        print(js['status'])
-        if js['status'] == "finished":
-            break
-
-    req = requests.post(url + '/' + path, headers=headers, data=json.dumps(param))
-    js = req.json()
-
-    headers = {'Authorization': auth_key, 'Content-Type': 'application/json'}
-
-    req = requests.get(url+'/score', headers=headers)
-    print(req.json()['score'])
-
-main(1)
-#main(2)
+kakaotaxi(1)
+#kakaotaxi(2)
