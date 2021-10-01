f102f6b1907da1c81853c235a00d5974070f479b
seororo358 <ssr3588@nate.com>
2021-10-01 15:13:43 +0900

    gravity_new

--- gravity.py
+++ gravity.py
@@ -1,6 +1,5 @@
-import time
-import sys
-sys.setrecursionlimit(10000)
+from collections import deque
+
 
 class Node(object):
     def __init__(self, key, data):
@@ -14,8 +13,12 @@ def sum_from_node(nodes, num):
     if len(nodes[num].child) == 0:
         return nodes[num].data
     else:
-        for i in nodes[num].child:
-            ans += sum_from_node(nodes, i)
+        q = deque()
+        q.extend(nodes[num].child)
+        while q:
+            ans += nodes[q[0]].data
+            q.extend(nodes[q.popleft()].child)
+
     return ans
 
 
@@ -23,14 +26,16 @@ def sec_query(parent, nodes, num, w):
     if nodes[num].key == 1:
         nodes[num].data = w
     else:
-        temp = nodes[parent[num]].data
-        nodes[num].data = temp
-        sec_query(parent, nodes, parent[num], w)
+        current_node = num
+        while current_node != 1:
+            nodes[current_node].data = nodes[parent[current_node]].data
+            current_node = parent[current_node]
+        nodes[current_node].data = w
 
 
 def solution(values, edges, queries):
     answer = []
-    nodes = [Node(0,0)]
+    nodes = [Node(0, 0)]
     parent = [0 for _ in range(len(values)+1)]
     i = 1
     for value in values:
@@ -39,8 +44,10 @@ def solution(values, edges, queries):
     '''for node in nodes:
         print(node.key, node.data)'''
     for edge in edges:
-        nodes[edge[0]].child.append(edge[1])
-        parent[edge[1]] = edge[0]
+        ma = max(edge)
+        mi = min(edge)
+        nodes[mi].child.append(ma)
+        parent[ma] = mi
 
     for query in queries:
         if query[1] == -1:
@@ -50,7 +57,9 @@ def solution(values, edges, queries):
 
     return answer
 
-start = time.time()
-print(solution([1,10,100,1000,10000], [[1,2],[1,3],[2,4],[2,5]],
-         [[1,-1],[2,-1],[3,-1],[4,-1],[5,-1],[4,1000],[1,-1],[2,-1],[3,-1],[4,-1],[5,-1],[2,1],[1,-1],[2,-1],[3,-1],[4,-1],[5,-1]]))
-print(time.time()-start)
+
+
+print(solution([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096],
+               [[10, 11], [13, 11], [12, 10], [10, 9], [8, 9], [1, 2], [2, 4], [2, 3], [5, 3], [6, 5], [5, 8], [7, 5]],
+         [[7,8192],[1, -1], [2, -1], [3, -1], [4, -1], [5, -1], [6, -1], [7, -1], [8, -1], [9, -1], [10, -1], [11, -1], [12, -1], [13, -1]]))
+
