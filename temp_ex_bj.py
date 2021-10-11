f41c4986b2fadce55699556e4f5e1ddb8289f125
seororo358 <ssr3588@nate.com>
2021-10-11 23:03:36 +0900

    temp

--- temp_ex_bj.py
+++ temp_ex_bj.py
@@ -1,33 +1,23 @@
-import heapq as hq
-import sys
-input = sys.stdin.readline
+import bisect as bi
+N, M = map(int, input().split())
+arr = []
+query = []
+answer = []
+for _ in range(N):
+    arr.append(int(input()))
 
-inf = int(1e9)
+for _ in range(M):
+    query.append(int(input()))
+arr.sort()
+for i in query:
+    temp = bi.bisect_left(arr, i)
+    if temp == len(arr):
+        answer.append(-1)
+    else:
+        if arr[temp] == i:
+            answer.append(temp)
+        else:
+            answer.append(-1)
 
-v, e = map(int, input().split())
-start = int(input())
-graph = [[] for _ in range(v+1)]
-dis = [inf] * (v+1)
-
-for _ in range(e):
-    u, v, w = map(int, input().split())
-    graph[u].append((v, w))
-
-q = []
-hq.heappush(q,(0,start))
-dis[start] = 0
-while q:
-    dist, now = hq.heappop(q)
-    if dis[now] < dist:
-        continue
-    for i in graph[now]:
-        cost = dist + i[1]
-        if cost < dis[i[0]]:
-            dis[i[0]] = cost
-            hq.heappush(q, (cost, i[0]))
-
-for i in range(1, len(dis)):
-    if dis[i] == inf:
-        print("INF")
-        continue
-    print(dis[i])
+for i in answer:
+    print(i)
