d999c5b17877aa3374f4511e26b2a6da36968e56
seororo358 <ssr3588@nate.com>
2021-10-27 23:56:34 +0900

    temp

--- temp_ex_bj.py
+++ temp_ex_bj.py
@@ -1,3 +1,22 @@
 import sys
-N, M = map(int, sys.stdin.readline().split())
 
+n, k = map(int, sys.stdin.readline().split())
+arr = [True] * (n+1)
+start = 2
+answer = 0
+while start <= n:
+    if not arr[start]:
+        start += 1
+        continue
+    for i in range(start, n+1):
+        if i % start or not arr[i]:
+            continue
+        else:
+            k -= 1
+            arr[i] = False
+            if k == 0:
+                answer = i
+                break
+    if answer > 0:
+        break
+print(answer)
