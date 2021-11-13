066ab7bdcc4033159a33db75d2c8f66833f185e7
seororo358 <ssr3588@nate.com>
2021-11-13 15:47:18 +0900

    temp

--- temp_ex_bj.py
+++ temp_ex_bj.py
@@ -1,14 +1,40 @@
 import sys
 input = sys.stdin.readline
-def update(idx, dif):
-    while(idx <= n):
-        tree[idx] += dif
-        idx += (idx & -idx)
+
+from collections import deque
+
+dx = [1, 0, 0, -1]
+dy = [0, 1, -1, 0]
 
 
-n, m, k = map(int, input().split())
-tree = [0] * n
-for i in range(n):
-    tree[i] = int(input())
-for _ in range(m+k):
-    a, b, c = map(int, input().split())
+def solution(seat):
+    answer = 0
+    l = len(seat)
+    for i in range(l):
+        seat[i] = list(seat[i])
+
+    for i in range(l):
+        for j in range(l):
+            if seat[i][j] == 'C':
+                step = 0
+                q = deque([(i, j, step)])
+                seat[i][j] = '-'
+                while q:
+                    x, y, st = q.popleft()
+                    for k in range(4):
+                        new_x = x + dx[k]
+                        new_y = y + dy[k]
+                        if new_x < 0 or new_y < 0 or new_x >= l or new_y >= l:
+                            continue
+                        if st == 2 and seat[new_x][new_y] == 'N':
+                            answer += 1
+                            seat[new_x][new_y] = '-'
+                            continue
+                        if seat[new_x][new_y] == 'N' or seat[new_x][new_y] == 'M':
+                            answer += 1
+                            seat[new_x][new_y] = '-'
+                        q.append((new_x, new_y, st + 1))
+
+    return answer
+
+print(solution(["M--NN-", "-M----", "-CC--M", "-N----", "N-M-C-", "-M----"]))
